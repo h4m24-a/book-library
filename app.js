013cefdd38986a49f9a6e05228bcb42fdb055331
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-05-12 22:02:34 +0100

    created functions to display form when clicking on add book button

---
+++ app.js
@@ -0,0 +1,24 @@
+const addbookbtn = document.getElementById("new-book-btn");
+const form = document.getElementById("form");
+const formwrap = document.getElementById("form-wrapper")
+
+
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(book) {
+  const newBook = new book("title, author, pages, read");
+  myLibrary.push(newBook);
+}
+
+// Button to show book form
+addbookbtn.addEventListener('click', displayForm);
+function displayForm () {
+    form.style.display = 'block';
+}
