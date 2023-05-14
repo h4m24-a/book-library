c1e9eb431e3a381a3133d234be60d1e57d038b85
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-05-14 19:37:30 +0100

    allows you to exit the form when clicking on the add book button

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const addbookbtn = document.getElementById("new-book-btn");
 const form = document.getElementById("form");
-const formwrap = document.getElementById("form-wrapper")
-const readbtn = document.getElementById("readbtn")
-
+const formwrap = document.getElementById("form-wrapper");
+const readbtn = document.getElementById("readbutton");
+const removebtn = document.getElementById("removebtn");
 
 let myLibrary = [];
 
@@ -19,15 +19,42 @@ function addBookToLibrary(book) {
 }
 
 // Button to show book form
-addbookbtn.addEventListener('click', displayForm);
-function displayForm () {
+addbookbtn.addEventListener("click", displayForm);
+
+function displayForm() {
+  if (form.style.display === 'none') {
+    // this SHOWS the form
     form.style.display = 'block';
+  } else {
+    // this HIDES the form
+    form.style.display = 'none';
+  }
 }
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 // Change status - Read or Not
-readbtn.addEventListener('click', function () {
-    readbtn.textContent = 'Not Read';
-  });
+// readbtn.addEventListener('click', readStatus);
+
+// function readStatus() {
+//   if (readbtn.innerHTML === "Read") {
+//     readbtn.innerHTML = "Not Read";
+//   } else {
+//     readbtn.innerHTML = "Read";
+//   }
+// }
+
