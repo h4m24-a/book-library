e9c719b7e0290ff655425079dd5b659875eae6f6
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-06-13 17:30:17 +0100

    fixed addtoLibrary bug and the submit button of form

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const addbookbtn = document.getElementById("new-book-btn");
 const form = document.getElementById("form");
-const formwrap = document.getElementById("form-wrapper");
-const readbtn = document.getElementById("readbutton");
-const removebtn = document.getElementById("removebtn");
+const bookTable = document.getElementById("booktable")
+
+
 
 let myLibrary = [];
 
@@ -13,10 +13,36 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-function addBookToLibrary(book) {
-  const newBook = new book("title, author, pages, read");
+function addBookToLibrary() {
+let title = document.getElementById("title").value
+let author = document.getElementById("author").value
+let pages = document.getElementById("pages").value
+let read = document.getElementById("read").checked
+let newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
+  console.log(newBook)
 }
+
+
+function renderBook() {
+  for (let i = 0; i < myLibrary.length; i ++) {
+    bookTable.innerHTML += 
+    `<tr> <td>${Book.title}</td> <td>${Book.author}</td> <td>${Book.title}</td> <td>${Book.author}</td> <td>${Book.pages}</td> <td>${Book.read}</td> </tr>`
+}
+
+
+
+}
+
+
+
+document.querySelector("#addbook").addEventListener("submit", function(event){
+  event.preventDefault();
+  addBookToLibrary()
+  console.log("button clicked")
+})
+
+
 
 // Button to show book form
 addbookbtn.addEventListener("click", displayForm);
@@ -30,31 +56,3 @@ function displayForm() {
     form.style.display = 'none';
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Change status - Read or Not
-// readbtn.addEventListener('click', readStatus);
-
-// function readStatus() {
-//   if (readbtn.innerHTML === "Read") {
-//     readbtn.innerHTML = "Not Read";
-//   } else {
-//     readbtn.innerHTML = "Read";
-//   }
-// }
-
