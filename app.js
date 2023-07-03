2167ba45f51ed8d57ac2be84afb8aef7b0744279
Hamza Ashraf <hamza.ashraf@live.co.uk>
2023-07-03 18:51:09 +0100

    refactored book  constructor to use class instead of plain constructor

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ const bookTable = document.getElementById("booktable");
 let myLibrary = [];
 
 
-function Book(title, author, pages, read) {
+class Book {
+  constructor (title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  }
 }
 
 
